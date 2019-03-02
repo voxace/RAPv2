3cfd1b0e35c80fc301554f1b0675919690a36404
voxace <dave@voxace.com>
2019-03-03 07:28:13 +1100

    Update generate.vue

--- client/pages/admin/generate.vue
+++ client/pages/admin/generate.vue
@@ -1,6 +1,6 @@
 <template>
   <object
-    data="http://192.168.1.232:3001/api/admin/posters.pdf"
+    data="http://mhsrap.com:3001/api/admin/posters.pdf"
     type="application/pdf"
     width="100%"
     height="800px"
@@ -8,7 +8,7 @@
     <p>
       It appears you don't have a PDF plugin for this browser. No biggie... you
       can
-      <a href="http://192.168.1.232:3001/api/admin/posters.pdf"
+      <a href="http://mhsrap.com:3001/api/admin/posters.pdf"
         >click here to download the PDF file.</a
       >
     </p>
